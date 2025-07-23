628c45f2c70ce3abcc153061c90eb42f069ec4ec
dcanchari <dtcancharir@gmail.com>
2025-07-23 00:43:49 -0500

    First commit

---
+++ src/main/java/com/project/sys_spelling_bee_contest_backend/SysSpellingBeeContestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.sys_spelling_bee_contest_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysSpellingBeeContestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysSpellingBeeContestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/sys_spelling_bee_contest_backend/SysSpellingBeeContestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sys_spelling_bee_contest_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysSpellingBeeContestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
