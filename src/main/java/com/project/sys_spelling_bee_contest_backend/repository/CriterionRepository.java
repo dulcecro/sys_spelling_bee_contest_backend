0f247068891f3211544cab4726ef07fea43d616f
dcanchari <dtcancharir@gmail.com>
2025-08-11 00:03:48 -0500

    Delete Entity Criterion

--- src/main/java/com/project/sys_spelling_bee_contest_backend/repository/CriterionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.sys_spelling_bee_contest_backend.repository;
-
-import com.project.sys_spelling_bee_contest_backend.model.Criterion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CriterionRepository extends JpaRepository<Criterion, Integer> {
-}
--- src/main/java/com/project/sys_spelling_bee_contest_backend/service/CriterionService.java
+++
@@ -1,9 +0,0 @@
-package com.project.sys_spelling_bee_contest_backend.service;
-
-import com.project.sys_spelling_bee_contest_backend.DTO.CriterionDTO;
-
-import java.util.List;
-
-public interface CriterionService {
-    List<CriterionDTO> listCriterion();
-}
